066686a6c8ace2550fcbc3762650f98b39e19984
ElouTTD <elouan.tatard@edu.devinci.fr>
2026-02-06 10:14:32 +0100

    Refactor CMS pricing input and calculation layout

--- pages/7_Constant_Maturity_Swap.py
+++ pages/7_Constant_Maturity_Swap.py
@@ -9,7 +9,6 @@ st.set_page_config(page_title="CMS Pricing", layout="wide")
 
 st.title("Constant Maturity Swap (CMS) Pricer")
 
-#INPUTS
 col1, col2 = st.columns([1, 2])
 with col1:
     st.subheader("Paramètres")
@@ -18,13 +17,10 @@ with col1:
     fix_rate = st.number_input("Taux Fixe (K) %", value=3.0) / 100
     cms_tenor = st.selectbox("Maturité Constante", ["CMS 10Y", "CMS 2Y"])
 
-#CALCUL
 if st.button("Calculer"):
-    # On appelle ton pricer
     pricer = CMSPricer(discount_curve=None, swap_surface=None)
     res = pricer.calculate_price(nominal, fix_rate, maturity, cms_tenor)
     
-    #RESULTAT
     with col2:
         st.metric("Prix Total (NPV)", f"{res['price']:,.2f} €")
         st.write(f"Jambe CMS : {res['leg_cms']:,.2f} € | Jambe Fixe : {res['leg_fix']:,.2f} €")
